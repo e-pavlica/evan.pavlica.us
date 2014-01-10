73f2a4344d4571d71283efb70a11f21853505f76
Evan Pavlica <evan@xicreative.net>
2014-01-09 18:37:17 -0800

    add comment.js to index and resume

--- xicreative.rb
+++ xicreative.rb
@@ -2,18 +2,16 @@
 require 'sinatra'
 require 'haml'
   
-class Xicreative < Sinatra::Application
   
-  get '/' do
-    haml :index, :format => :html5
-  end
-  
-  get '/resume' do
-    haml :resume, :format => :html5
-  end
-  
-  get '/about' do
-    haml :about, :format => :html5
-  end
-  
+get '/' do
+  haml :index, :format => :html5
 end
+
+get '/resume' do
+  haml :resume, :format => :html5
+end
+
+get '/about' do
+  haml :about, :format => :html5
+end
+  
