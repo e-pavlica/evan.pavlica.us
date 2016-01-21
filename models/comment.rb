9b2843e96edb62488657be4e6c4a6beabed0c5b1
Evan Pavlica <evan@xicreative.net>
2016-01-20 19:36:48 -0800

    Out with the old

--- models/comment.rb
+++
@@ -1,7 +0,0 @@
-class Comment
-  include Mongoid::Document
-
-  field :text
-  belongs_to :user
-  belongs_to :post
-end
--- public/angular/services/post.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-app.factory('Post', function($resource) {
-  return $resource('http://api.'+ location.host + '/posts/:id.json');
-});
